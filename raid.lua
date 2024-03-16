69daf8347e788cbc0b61b2c033ef57b768fbe255
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-16 12:51:13 +0500

    Update raid.lua

--- raid.lua
+++ raid.lua
@@ -1,5 +1,7 @@
-task.wait(15)
-local teleportService = game:GetService("TeleportService")
+local runService = game:GetService("RunService")
+runService.Stepped:Connect(function()
+    if game.Loaded then
+        local teleportService = game:GetService("TeleportService")
 local placeId = 16644455867
 local player = game.Players.LocalPlayer
 
@@ -14,21 +16,31 @@ end
 else
 print("Player not found.")
 end
+    end
+end)
 
-task.wait(15)
-while wait() do
+
+
+local runService = game:GetService("RunService")
+runService.Stepped:Connect(function()
+    if game.Loaded then
+        while wait() do
 local args = {
 [1] = "Tsunami"
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
 end
+    end
+end)
 
-task.wait(10)
-while wait() do
+
+
+local runService = game:GetService("RunService")
+runService.Stepped:Connect(function()
+    if game.Loaded then
+        while wait(1) do
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
 end
+    end
+end)
 
-task.wait(20)
-while wait() do
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
-end
