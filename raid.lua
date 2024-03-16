1cd6a45a2124e56dde5f76eda73b949202d5f080
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-16 12:28:55 +0500

    Create raid.lua

---
+++ raid.lua
@@ -0,0 +1,29 @@
+task.wait(20)
+local teleportService = game:GetService("TeleportService")
+local placeId = 16644455867
+local player = game.Players.LocalPlayer
+
+if player then
+local currentPlaceId = game.PlaceId
+
+if currentPlaceId ~= placeId then
+    teleportService:Teleport(placeId, player)
+else
+    print("Already in the destination place.")
+end
+else
+print("Player not found.")
+end
+
+task.wait(20)
+while wait() do
+local args = {
+[1] = "Tsunami"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
+end
+
+task.wait(20)
+while wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
+end
