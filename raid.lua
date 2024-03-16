128ada66f35bbece2eee701fd78727604384716d
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-16 13:46:18 +0500

    Update raid.lua

--- raid.lua
+++ raid.lua
@@ -1,46 +1,43 @@
 local runService = game:GetService("RunService")
+
+-- Teleport to a specific place ID when the game has finished loading
 runService.Stepped:Connect(function()
     if game.Loaded then
         local teleportService = game:GetService("TeleportService")
-local placeId = 16644455867
-local player = game.Players.LocalPlayer
+        local placeId = 16644455867
+        local player = game.Players.LocalPlayer
 
-if player then
-local currentPlaceId = game.PlaceId
+        if player then
+            local currentPlaceId = game.PlaceId
 
-if currentPlaceId ~= placeId then
-    teleportService:Teleport(placeId, player)
-else
-    print("Already in the destination place.")
-end
-else
-print("Player not found.")
-end
+            if currentPlaceId ~= placeId then
+                teleportService:Teleport(placeId, player)
+            else
+                print("Already in the destination place.")
+            end
+        else
+            print("Player not found.")
+        end
     end
 end)
 
-
-
-local runService = game:GetService("RunService")
+-- Use a remote event to fire a server-side function every frame
 runService.Stepped:Connect(function()
     if game.Loaded then
         while wait() do
-local args = {
-[1] = "Tsunami"
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
-end
+            local args = {
+                [1] = "Tsunami"
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
+        end
     end
 end)
 
-
-
-local runService = game:GetService("RunService")
+-- Use a remote event to fire a server-side function every 5 seconds
 runService.Stepped:Connect(function()
     if game.Loaded then
         while wait(5) do
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
-end
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
+        end
     end
 end)
-
