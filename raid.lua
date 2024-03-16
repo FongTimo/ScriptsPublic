a8f726f74be5b8c104810fae9bb3e18c186fc34e
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-16 13:54:45 +0500

    Update raid.lua

--- raid.lua
+++ raid.lua
@@ -1,43 +1,49 @@
 local runService = game:GetService("RunService")
 
--- Teleport to a specific place ID when the game has finished loading
 runService.Stepped:Connect(function()
-    if game.Loaded then
-        local teleportService = game:GetService("TeleportService")
-        local placeId = 16644455867
-        local player = game.Players.LocalPlayer
+    while wait() do
+        if game:IsLoaded() then
+            local teleportService = game:GetService("TeleportService")
+            local placeId = 16644455867
+            local player = game.Players.LocalPlayer
 
-        if player then
-            local currentPlaceId = game.PlaceId
+            if player then
+                local currentPlaceId = game.PlaceId
 
-            if currentPlaceId ~= placeId then
-                teleportService:Teleport(placeId, player)
+                if currentPlaceId ~= placeId then
+                    teleportService:Teleport(placeId, player)
+                else
+                    print("Already in the destination place.")
+                end
             else
-                print("Already in the destination place.")
+                print("Player not found.")
             end
-        else
-            print("Player not found.")
+            break
         end
     end
 end)
 
--- Use a remote event to fire a server-side function every frame
 runService.Stepped:Connect(function()
-    if game.Loaded then
-        while wait() do
-            local args = {
-                [1] = "Tsunami"
-            }
-            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
+    while wait() do
+        if game:IsLoaded() then
+            while wait() do
+                local args = {
+                    [1] = "Tsunami"
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
+            end
+            break
         end
     end
 end)
 
--- Use a remote event to fire a server-side function every 5 seconds
 runService.Stepped:Connect(function()
-    if game.Loaded then
-        while wait(5) do
-            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
+    while wait() do
+        if game:IsLoaded() then
+            while wait(5) do
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
+            end
+            break
         end
     end
 end)
