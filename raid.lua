cc35351315207ad7c2adf3b0f758d144f5d0dc55
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-16 12:41:52 +0500

    Update raid.lua

--- raid.lua
+++ raid.lua
@@ -1,4 +1,4 @@
-task.wait(20)
+task.wait(15)
 local teleportService = game:GetService("TeleportService")
 local placeId = 16644455867
 local player = game.Players.LocalPlayer
@@ -21,6 +21,11 @@ local args = {
 [1] = "Tsunami"
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SkillHolder"):FireServer(unpack(args))
+end
+
+task.wait(10)
+while wait() do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Ready"):FireServer()
 end
 
 task.wait(20)
