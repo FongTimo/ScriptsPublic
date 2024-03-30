9167f74930d7c5008c73c97738567b90a10a01ca
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-30 09:59:04 +0500

    Update raid1.lua

--- raid1.lua
+++ raid1.lua
@@ -90,19 +90,10 @@ end)
 
 runService.Stepped:Connect(function()
     while wait(1) do
-        if game:IsLoaded(3) then
-while true do
-    -- Получаем текущее состояние.
-    local runService = game:GetService("RunService")
-    local state = runService.Stepped:Wait()
-
-    -- Проверяем, были ли мы дисконнекчены.
-    if state.NetworkStatus and state.NetworkStatus == Enum.NetworkStatus.Disconnected then
-        -- Если мы дисконнекчены, пытаемся переподключиться.
-        game:GetService("NetworkClient"):JoinServer(game.PlaceId)
-    end
-end
+        if game:IsLoaded(260) then
+                game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
             break
         end
     end
 end)
+
