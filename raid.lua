18b555fec79233f68a8b956c4703f6911093423b
FongTimo <155631299+FongTimo@users.noreply.github.com>
2024-03-30 07:00:13 +0500

    Update raid.lua

--- raid.lua
+++ raid.lua
@@ -2,7 +2,7 @@ local runService = game:GetService("RunService")
 
 runService.Stepped:Connect(function()
     while wait() do
-        if game:IsLoaded(3) then
+        if game:IsLoaded(5) then
             local teleportService = game:GetService("TeleportService")
             local placeId = 16644455867
             local player = game.Players.LocalPlayer
